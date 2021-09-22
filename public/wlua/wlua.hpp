d39740bac9acceb29162a9460903a1ad6e2f2013
bachelorwang <whc@bachelor-studio.com>
2021-09-22 21:51:16 +0800

    change naming rule

--- public/wlua/wlua.hpp
+++ public/wlua/wlua.hpp
@@ -10,27 +10,27 @@ namespace wlua {
 
 namespace details {
 
-template <typename _signature, _signature* _fp>
+template <typename TSignature, TSignature* TFp>
 struct function;
 
-template <typename _ret, typename... _args, _ret (*_fp)(_args...)>
-struct function<_ret(_args...), _fp> {
-  using result_type = _ret;
-  using parameter_types = std::tuple<_args...>;
-  static constexpr size_t parameter_count = sizeof...(_args);
-  using type = _ret (*)(_args...);
-  static constexpr _ret (*pointer)(_args...) = _fp;
+template <typename TRet, typename... TArgs, TRet (*TFp)(TArgs...)>
+struct function<TRet(TArgs...), TFp> {
+  using result_type = TRet;
+  using parameter_types = std::tuple<TArgs...>;
+  static constexpr size_t parameter_count = sizeof...(TArgs);
+  using type = TRet (*)(TArgs...);
+  static constexpr TRet (*pointer)(TArgs...) = TFp;
 };
 
-template <typename _arg>
-inline auto pop_arg(_arg& arg, lua_State* handle, size_t i)
-    -> std::enable_if_t<std::is_integral_v<_arg>, void> {
+template <typename TArg>
+inline auto pop_arg(TArg& arg, lua_State* handle, size_t i)
+    -> std::enable_if_t<std::is_integral_v<TArg>, void> {
   arg = lua_tointeger(handle, i);
 }
 
-template <typename _arg>
-inline auto pop_arg(_arg& arg, lua_State* handle, size_t i)
-    -> std::enable_if_t<std::is_floating_point_v<_arg>, void> {
+template <typename TArg>
+inline auto pop_arg(TArg& arg, lua_State* handle, size_t i)
+    -> std::enable_if_t<std::is_floating_point_v<TArg>, void> {
   arg = lua_tonumber(handle, i);
 }
 
@@ -38,57 +38,57 @@ inline auto pop_arg(const char*& arg, lua_State* handle, size_t i) {
   arg = lua_tostring(handle, i);
 }
 
-template <typename _args, size_t... _indexes>
+template <typename TArgs, size_t... TIndexes>
 inline void pop_all_args(lua_State* handle,
-                                _args& args,
-                                std::index_sequence<_indexes...>) {
-  (pop_arg(std::get<_indexes>(args), handle, _indexes + 1), ...);
+                                TArgs& args,
+                                std::index_sequence<TIndexes...>) {
+  (pop_arg(std::get<TIndexes>(args), handle, TIndexes + 1), ...);
 }
 
-template <typename _function, typename>
+template <typename TFunction, typename>
 struct invoker;
 
-template <typename _function>
-struct invoker<_function, std::true_type> {
-  template <size_t... _indexes>
+template <typename TFunction>
+struct invoker<TFunction, std::true_type> {
+  template <size_t... TIndexes>
   static int invoke(lua_State* handle,
-                    typename _function::parameter_types&& args,
-                    std::index_sequence<_indexes...>) {
-    (*_function::pointer)(std::get<_indexes>(args)...);
+                    typename TFunction::parameter_types&& args,
+                    std::index_sequence<TIndexes...>) {
+    (*TFunction::pointer)(std::get<TIndexes>(args)...);
     return 0;
   }
 };
 
-template <typename _function>
-struct invoker<_function, std::false_type> {
-  template <size_t... _indexes>
+template <typename TFunction>
+struct invoker<TFunction, std::false_type> {
+  template <size_t... TIndexes>
   static int invoke(lua_State* handle,
-                    typename _function::parameter_types&& args,
-                    std::index_sequence<_indexes...>) {
-    auto result = (*_function::pointer)(std::get<_indexes>(args)...);
+                    typename TFunction::parameter_types&& args,
+                    std::index_sequence<TIndexes...>) {
+    auto result = (*TFunction::pointer)(std::get<TIndexes>(args)...);
     lua_push_result(handle, result);
     return 1;
   }
 
  private:
-  template <typename _type>
-  static inline auto lua_push_result(lua_State* handle, _type result)
-      -> std::enable_if_t<std::is_integral_v<_type>> {
+  template <typename TType>
+  static inline auto lua_push_result(lua_State* handle, TType result)
+      -> std::enable_if_t<std::is_integral_v<TType>> {
     lua_pushinteger(handle, result);
   }
 };
 
-template <typename _function>
+template <typename TFunction>
 struct lua_bind {
   static int invoke(lua_State* handle) {
-    typename _function::parameter_types args;
+    typename TFunction::parameter_types args;
     constexpr auto indexes =
-        std::make_index_sequence<_function::parameter_count>{};
+        std::make_index_sequence<TFunction::parameter_count>{};
     pop_all_args(handle, args, indexes);
     using is_void =
-        typename std::is_same<typename _function::result_type, void>::type;
-    using _invoker = invoker<_function, is_void>;
-    return _invoker::invoke(handle, std::move(args), indexes);
+        typename std::is_same<typename TFunction::result_type, void>::type;
+    using TInvoker = invoker<TFunction, is_void>;
+    return TInvoker::invoke(handle, std::move(args), indexes);
   }
 };
 
@@ -104,48 +104,48 @@ inline void push_arg(lua_State* handle, const char* arg) {
   lua_pushstring(handle, arg);
 }
 
-template <typename... _args>
-inline void lua_invoke(lua_State* handle, const char* name, _args&&... args) {
+template <typename... TArgs>
+inline void lua_invoke(lua_State* handle, const char* name, TArgs&&... args) {
   lua_getglobal(handle, name);
   ((push_arg(handle, args)), ...);
 }
 
-template <typename _result_type>
+template <typename TResultType>
 inline auto pop_result(lua_State* handle)
-    -> std::enable_if_t<std::is_integral_v<_result_type>, lua_Integer> {
+    -> std::enable_if_t<std::is_integral_v<TResultType>, lua_Integer> {
   return lua_tointeger(handle, -1);
 }
 
-template <typename _result_type>
+template <typename TResultType>
 inline auto pop_result(lua_State* handle)
-    -> std::enable_if_t<std::is_floating_point_v<_result_type>, lua_Number> {
+    -> std::enable_if_t<std::is_floating_point_v<TResultType>, lua_Number> {
   return lua_tonumber(handle, -1);
 }
 
-template <typename _result_type>
+template <typename TResultType>
 inline auto pop_result(lua_State* handle)
-    -> std::enable_if_t<std::is_same_v<const char*, _result_type>,
+    -> std::enable_if_t<std::is_same_v<const char*, TResultType>,
                         const char*> {
   return lua_tostring(handle, -1);
 }
 
-template <typename _result_type>
+template <typename TResultType>
 inline auto pop_result(lua_State* handle)
-    -> std::enable_if_t<std::is_same_v<void, _result_type>, void> {
+    -> std::enable_if_t<std::is_same_v<void, TResultType>, void> {
   return;
 }
 
-template <typename _signature>
+template <typename TSignature>
 struct c_bind;
 
-template <typename _ret, typename... _args>
-struct c_bind<_ret(_args...)> {
-  lua_State* _handle = nullptr;
-  const char* _func_name = nullptr;
-  _ret operator()(_args&&... args) {
-    lua_invoke(_handle, _func_name, std::forward<_args>(args)...);
-    lua_pcall(_handle, sizeof...(args), 1, 0);
-    return pop_result<_ret>(_handle);
+template <typename TRet, typename... TArgs>
+struct c_bind<TRet(TArgs...)> {
+  lua_State* handle_ = nullptr;
+  const char* func_name_ = nullptr;
+  TRet operator()(TArgs... args) {
+    lua_invoke(handle_, func_name_, std::forward<TArgs>(args)...);
+    lua_pcall(handle_, sizeof...(args), 1, 0);
+    return pop_result<TRet>(handle_);
   }
 };
 
@@ -154,15 +154,15 @@ struct c_bind<_ret(_args...)> {
 #define WLUA_FUNCTION(fp) \
   ::wlua::details::lua_bind<::wlua::details::function<decltype(fp), fp>>
 
-template <typename _bind>
+template <typename TBind>
 void add_function(lua_State* handle, const char* name) {
-  lua_pushcfunction(handle, &_bind::invoke);
+  lua_pushcfunction(handle, &TBind::invoke);
   lua_setglobal(handle, name);
 }
 
-template <typename _signature>
-details::c_bind<_signature> get_function(lua_State* handle, const char* name) {
-  return details::c_bind<_signature>{handle, name};
+template <typename TSignature>
+details::c_bind<TSignature> get_function(lua_State* handle, const char* name) {
+  return details::c_bind<TSignature>{handle, name};
 }
 
 }  // namespace wlua
